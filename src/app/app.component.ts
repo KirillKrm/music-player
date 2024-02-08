c3afb577fb194b22c3b5b755d5fa7fd6d35f8e8e
KirillKrm <kirillkarmazin2301@gmail.com>
2024-02-08 19:39:05 +0200

    feat: added play button and volume slidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'music-player';
+  audio = new Audio()
+  volume = 1
+
+  playSound() {
+    this.audio.src = '../assets/thepower.wav'
+    this.audio.load()
+    this.audio.play()
+  }
+
+  changeVolume(value: string) {
+    this.volume = Number(value) / 100
+    this.audio.volume = this.volume
+  }
 }
