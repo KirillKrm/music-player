dfed7281ba74f7e899a59bc20f59b9935bee131e
KirillKrm <kirillkarmazin2301@gmail.com>
2024-02-11 19:37:35 +0200

    feat: added visualizer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 
 @Component({
@@ -8,11 +8,58 @@ import { RouterOutlet } from '@angular/router'
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  fileUrl = ''
-  isPlay = false
+export class AppComponent implements AfterViewInit {
+  @ViewChild('audioRef') audioRef!: ElementRef<HTMLMediaElement>
+  @ViewChild('visualizerRef') visualizerRef!: ElementRef<HTMLCanvasElement>
 
-  playSound(audioRef: HTMLAudioElement) {
+  public fileUrl = ''
+  public isPlay = false
+
+  ngAfterViewInit() {
+    const audioContext = new AudioContext()
+    const audioSource = audioContext.createMediaElementSource(
+      this.audioRef.nativeElement,
+    )
+    const analyser = audioContext.createAnalyser()
+
+    audioSource.connect(analyser)
+    audioSource.connect(audioContext.destination)
+
+    const canvasCtx = this.visualizerRef.nativeElement.getContext('2d')
+    const bufferLength = analyser.frequencyBinCount
+    const dataArray = new Uint8Array(bufferLength)
+
+    const WIDTH = this.visualizerRef.nativeElement.width
+    const HEIGHT = this.visualizerRef.nativeElement.height
+
+    const barWidth = (WIDTH / bufferLength) * 2.5
+    let barHeight
+    let x = 0
+
+    function renderFrame() {
+      requestAnimationFrame(renderFrame)
+
+      x = 0
+
+      analyser.getByteFrequencyData(dataArray)
+
+      canvasCtx!.fillStyle = '#000'
+      canvasCtx!.fillRect(0, 0, WIDTH, HEIGHT)
+
+      for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i]
+
+        canvasCtx!.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)'
+        canvasCtx!.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2)
+
+        x += barWidth + 1
+      }
+    }
+
+    renderFrame()
+  }
+
+  public playSound(audioRef: HTMLAudioElement) {
     if (this.fileUrl) {
       if (this.isPlay) {
         audioRef.pause()
@@ -23,11 +70,14 @@ export class AppComponent {
     }
   }
 
-  onChangeVolume(audioRef: HTMLAudioElement, volumeRef: HTMLInputElement) {
+  public onChangeVolume(
+    audioRef: HTMLAudioElement,
+    volumeRef: HTMLInputElement,
+  ) {
     audioRef.volume = Number(volumeRef.value) / 100
   }
 
-  onUpdateProgress(
+  public onUpdateProgress(
     audioRef: HTMLAudioElement,
     progressRef: HTMLProgressElement,
   ) {
@@ -35,7 +85,9 @@ export class AppComponent {
     if (isFinite(currentProgress)) progressRef.value = currentProgress
   }
 
-  onFileSelected(fileRef: HTMLInputElement) {
+  public onFileSelected(fileRef: HTMLInputElement) {
     if (fileRef.files) this.fileUrl = URL.createObjectURL(fileRef.files[0])
   }
+
+  public draw() {}
 }
